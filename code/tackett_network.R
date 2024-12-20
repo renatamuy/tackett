7075e1f342338b1398afdfce3ebcbd2dad2d43d2
Marco Mello <marmello@usp.br>
2024-12-20 07:17:05 -0300

    Tripartite potting

--- code/tackett_network.R
+++ code/tackett_network.R
@@ -31,6 +31,16 @@ if(!require(ggplot2)){
   library(ggplot2)
 }
 
+if(!require(ggraph)){
+  install.packages("ggraph")
+  library(ggraph)
+}
+
+if(!require(ggrepel)){
+  install.packages("ggrepel")
+  library(ggrepel)
+}
+
 if(!require(htmlwidgets)){
   install.packages("htmlwidgets")
   library(htmlwidgets)
@@ -46,14 +56,14 @@ if(!require(networkD3)){
   library(networkD3)
 }
 
-if(!require(PhantomJS)){
-  install.packages("PhantomJS")
-  library(PhantomJS)
-}
-
 if(!require(tidyverse)){
   install.packages("tidyverse")
   library(tidyverse)
+}
+
+if(!require(tidygraph)){
+  install.packages("tidygraph")
+  library(tidygraph)
 }
 
 if(!require(webshot)){
@@ -104,7 +114,7 @@ unique(df$body.part)
 unique(df$cures)
 
 
-# Prep
+# Network version 1
 
 df <- df %>%
   filter(species != "unknown")
@@ -143,7 +153,7 @@ edge_attr(g1)
 plot(g1)
 
 
-# Alternative method for constructing the graph as tripartite
+# Network version 2
 
 df2 <- tibble(
   species = df$species,
@@ -165,7 +175,8 @@ if (length(unique_species) == 0 || length(unique_body_part) == 0 || length(uniqu
 
 # Combine all nodes
 all_vertices <- c(unique_species, unique_body_part, unique_cures)
-
+head(all_vertices)
+tail(all_vertices)
 
 # Create the node types
 vertex_types <- c(
@@ -187,7 +198,8 @@ vertex_list <- data.frame(
 
 # View the node list
 nodes2 <- vertex_list
-nodes2
+head(nodes2)
+tail(nodes2)
 
 duplicates <- duplicated(nodes2$name)
 duplicates
@@ -195,13 +207,11 @@ nodes2$name == "unknown"
 nodes2$name[duplicates] <- paste0(nodes2$name[duplicates], "_", seq_along(nodes2$name[duplicates]))
 nodes2
 
-
 links2 <- df2 %>%
   count(species, body.part, cures) %>%
   rename(value = n) %>%
   pivot_longer(cols = c(body.part, cures), names_to = "target_type", values_to = "target") %>%
   select(source = species, target, value)
-
 links2
 
 g2 <- graph_from_data_frame(d = links2, vertices = nodes2, directed = TRUE)
@@ -211,6 +221,50 @@ edge_attr(g2)
 plot(g2)
 
 
+# Network version 3
+
+colnames(df)
+
+edges_set1_set2 <- data.frame(from = df$species, to = df$body.part)
+edges_set2_set3 <- data.frame(from = df$body.part, to = df$cures)
+edges3 <- rbind(edges_set1_set2, edges_set2_set3)
+head(edges3)
+
+# Create graph
+g3 <- graph_from_data_frame(edges3, directed = FALSE)
+g3
+
+# Assign node groups
+V(g3)$group <- ifelse(V(g3)$name %in% df$species, 1,
+                      ifelse(V(g3)$name %in% df$body.part, 2, 3))
+V(g3)$group
+V(g3)$level <- V(g3)$group
+
+vertex_attr(g3)
+edge_attr(g3)
+
+
+# Set node and link properties
+
+V(g3)$type <- ifelse(V(g3)$group == 1, "species", "variable")
+V(g3)$type <- ifelse(V(g3)$group == 2, "body.part", V(g3)$type)
+V(g3)$type <- ifelse(V(g3)$group == 3, "cures", V(g3)$type)
+
+V(g3)$color <- ifelse(V(g3)$type == "species", "#DCC949", "#CD8862")
+V(g3)$color <- ifelse(V(g3)$type == "body.part", "#7D9D33", V(g3)$color)
+
+vertex_attr(g3)
+
+E(g3)$weight <- 1
+E(g3)$width <- E(g3)$weight / max(E(g3)$weight) * 2  # Scale link widths
+E(g3)$curved <- 0.2 
+E(g3)$color <- V(g3)$color[ends(g3, es = E(g3), names = FALSE)[, 2]] #Fixed!
+
+edge_attr(g3)
+
+plot(g3)
+
+
 ################################ SANKEY ########################################
 
 
@@ -218,6 +272,7 @@ sankey <- links %>%
   e_charts() %>%
   e_sankey(links, source = source, target = target, value = value) %>%
   e_theme("caravan")
+sankey
 
 # Save chart as HTML
 html_file <- "../figures/sankey.html"
@@ -228,18 +283,32 @@ png_file <- "../figures/tackett_network_sankey.png"
 webshot(html_file, png_file, vwidth = 3000, vheight = 2000)
 
 
-################################ MULTILAYER ####################################
+################################ ALLUVIAL ######################################
 
+# This is my favorite so far, but I still want to improve its visualization,
+# especially regarding node label overlap. We could also change the grey tone
+# of the strata.
+
+png(file = "../figures/tackett_network_alluvial.png", 
+    width = 6000, 
+    height = 2000, 
+    unit='px', 
+    res = 300, 
+    bg = "white")
 
 ggplot(data = df,
        aes(axis1 = species, axis2 = body.part, axis3 = cures)) +
   geom_alluvium(aes(fill = species), width = 1/12) +
   geom_stratum(width = 1/12, fill = "grey", color = "black") +
-  geom_text(stat = "stratum", aes(label = after_stat(stratum))) +
+  geom_text(stat = "stratum", 
+            aes(label = after_stat(stratum)),
+            size = 2) +
   scale_x_discrete(limits = c("species", "body.part", "cures"), expand = c(0.15, 0.15)) +
   labs(title = "Alluvial Plot of Species, Body Parts, and Cures",
        y = "Frequency") +
   theme_minimal()
+
+dev.off()
 
 
 ################################ TRIPARTITE ####################################
@@ -262,47 +331,6 @@ source("../code/layout_tripartite_type.R")
 source("../code/layout_tripartite_level.R")
 
 
-# Build the network
-
-colnames(df)
-
-edges_set1_set2 <- data.frame(from = df$species, to = df$body.part)
-edges_set2_set3 <- data.frame(from = df$body.part, to = df$cures)
-edges2 <- rbind(edges_set1_set2, edges_set2_set3)
-head(edges2)
-
-# Create graph
-g2 <- graph_from_data_frame(edges2, directed = FALSE)
-g2
-
-# Assign node groups
-V(g2)$group <- ifelse(V(g2)$name %in% df$species, 1,
-                      ifelse(V(g2)$name %in% df$body.part, 2, 3))
-V(g2)$group
-V(g2)$level <- V(g2)$group
-
-vertex_attr(g2)
-
-
-# Set node and link properties
-
-V(g2)$type <- ifelse(V(g2)$group == 1, "species", "variable")
-V(g2)$type <- ifelse(V(g2)$group == 2, "body.part", V(g2)$type)
-V(g2)$type <- ifelse(V(g2)$group == 3, "cures", V(g2)$type)
-
-V(g2)$color <- ifelse(V(g2)$type == "species", "#DCC949", "#CD8862")
-V(g2)$color <- ifelse(V(g2)$type == "body.part", "#7D9D33", V(g2)$color)
-
-vertex_attr(g2)
-
-E(g2)$weight <- E(g2)$value
-E(g2)$width <- E(g2)$weight / max(E(g2)$weight) * 10  # Scale link widths
-E(g2)$curved <- 0.2 
-E(g2)$color <- V(g2)$color[ends(g2, es = E(g2), names = FALSE)[, 2]] #Fixed!
-
-edge_attr(g2)
-
-
 # Tripartite graph
 
 png(file = "../figures/tackett_network_tripartite.png", 
@@ -315,18 +343,16 @@ png(file = "../figures/tackett_network_tripartite.png",
 par(mar = c(3, 3, 3, 3)) 
 
 plot(
-  g2,
+  g3,
   vertex.label.family = "Arial", 
   vertex.label.cex = 0.5,   
   layout = layout_tripartite_level, 
   vertex.size = 10,  
   vertex.label.cex = 1.5,  
   vertex.label.color = "gray30", 
-  #vertex.label.dist = 1.9,
   vertex.frame.color = "white",  
-  edge.width = E(g2)$width,
-  #edge.color = "gray63",
-  edge.color = E(g2)$color,
+  edge.width = E(g3)$weight,
+  edge.color = E(g3)$color,
   edge.arrow.size = 0,
   main = "Fruit bat costs and benefits per state"
 )
@@ -346,18 +372,16 @@ png(file = "../figures/tackett_network_circular.png",
 par(mar = c(3, 3, 3, 3)) 
 
 plot(
-  g2,
+  g3,
   vertex.label.family = "Arial", 
   vertex.label.cex = 0.5,   
   layout = layout_in_circle, 
   vertex.size = 10,  
   vertex.label.cex = 1.5,  
   vertex.label.color = "gray30", 
-  #vertex.label.dist = 1.9,
   vertex.frame.color = "white",  
-  edge.width = E(g2)$width,
-  #edge.color = "gray63",
-  edge.color = E(g2)$color,
+  edge.width = E(g3)$weight,
+  edge.color = E(g3)$color,
   edge.arrow.size = 0,
   main = "Fruit bat costs and benefits per state"
 )
